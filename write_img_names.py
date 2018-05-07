c311f78613f47bbbb9b36e3b57818eb24472f71c
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2018-05-07 18:33:16 -0300

    write name

--- write_img_names.py
+++ write_img_names.py
@@ -1,7 +1,8 @@
-import argparse, glob, os
+import argparse, glob, os, numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", type=str, help="images path to write in the file")
+parser.add_argument("-t", "--percentage", type=float, help="percentage for training")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
 
@@ -10,6 +11,8 @@ args = parser.parse_args()
 
 if args.path:
 	files = glob.glob(os.path.join(args.path, '*.jpg'))
+	np.random.shuffle(files)
+
 	if files:
 
 		file = open("train.txt","w") 
