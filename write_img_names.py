dba1a8fbb02ee40e6c50a719bba9cc48539bcf47
Leandro Bezerra Marinho <leandrobezerramarinho@gmail.com>
2018-05-08 10:29:29 -0300

    write img names with porcentage

--- write_img_names.py
+++ write_img_names.py
@@ -1,8 +1,8 @@
-import argparse, glob, os, numpy as np
+import argparse, glob, os, numpy as np, math
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", type=str, help="images path to write in the file")
-parser.add_argument("-ptrain", "--ptrain", type=float, help="percentage for training")
+parser.add_argument("-ptrain", "--ptrain", type=float, help="percentage for training between 0 and 1")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
 
@@ -13,21 +13,32 @@ if args.path:
 	files = glob.glob(os.path.join(args.path, '*.jpg'))
 	np.random.shuffle(files)
 
-	print(args.percentage)
+	ptrain = 0.95
+	if args.ptrain:
+		ptrain = args.ptrain
 
-	if files:
 
-		file = open("train.txt","w") 
-
+	if files:		
+		file_train = open("train.txt","w")
+		file_test = open("test.txt","w")		
+		
+		cont = 1
 		for filename in files:
 			filename = "data/img/{}".format(os.path.basename(filename))
 			if args.verbose:
-			    print("writing '{}'".format(filename))
+				type_file = ("train" if cont <= math.ceil(len(files)*ptrain) else "test")
+				print("writing in {}.txt '{}'".format(type_file, filename))
 
-			file.write("{}\n".format(filename))
+			if cont <= math.ceil(len(files)*ptrain):
+				file_train.write("{}\n".format(filename))
+			else:
+				file_test.write("{}\n".format(filename))
 
-		file.close()
-		print("done")
+			cont += 1
 
+		file_train.close()
+		file_test.close()
+
+		print("Num train: \t{}\nNum test: \t{}".format(int(math.ceil(len(files)*ptrain)), int(len(files) - math.ceil(len(files)*(ptrain)))))
 	else:
 		print("No images")
